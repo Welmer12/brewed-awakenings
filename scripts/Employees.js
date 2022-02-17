848972cdccf5e86316f8e1cb98cbc15f763a259d
Will Elmer <willpelmer12@gmail.com>
2022-02-16 21:56:41 -0600

    debugger and window alert

---
+++ scripts/Employees.js
@@ -0,0 +1,30 @@
+import { getEmployees, getOrders } from "./database.js";
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  if (itemClicked.id.startsWith("employee")) {
+    const [, employeeId] = itemClicked.id.split("--");
+
+    for (const employee of employees)
+      if (employee.id === parseInt(employeeId)) {
+        const matchedOrders = orders.filter(
+          (o) => o.employeeId === employee.id
+        );
+        window.alert(`${employee.name} sold ${matchedOrders.length} products`);
+      }
+  }
+});
+
+const employees = getEmployees();
+const orders = getOrders();
+export const Employees = () => {
+  let html = "<ul>";
+
+  for (const employee of employees) {
+    html += `<li id="employee--${employee.id}">${employee.name}</li>`;
+  }
+
+  html += "</ul>";
+
+  return html;
+};
